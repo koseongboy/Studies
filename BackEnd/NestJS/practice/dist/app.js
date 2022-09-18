0a446532b5008b449828feaf2153940af74c734c
Koseongboy <koseonghyeon08@gmail.com>
2022-09-18 21:50:06 +0900

    NestJS study strart (9/18)

---
+++ BackEnd/NestJS/practice/dist/app.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+var app = express();
+var port = 8000;
+app.get("/", function (req, res) {
+    console.log(req);
+    res.send({ hello: "world" });
+});
+app.listen(port, function () {
+    console.log("Example app listening on port http://localhost:" + port);
+});
+//# sourceMappingURL=app.js.map
---
+++ BackEnd/NestJS/practice/src/app.ts
@@ -0,0 +1,14 @@
+import * as express from "express";
+
+const app: express.Express = express();
+
+const port: number = 8000;
+
+app.get("/", (req: express.Request, res: express.Response) => {
+  console.log(req);
+  res.send({ hello: "world" });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
